57786150b85898a404cd3449f27857d3f58e26c6
payloads <payloads@aliyun.com>
2017-11-27 20:55:01 +0800

    修复安装文件

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install default-libmysqlclient-dev -y
 apt-get install libldns-dev -y
-
+g++ HeaderFile/controller_class.h HeaderFile/dns_class.h HeaderFile/global.h HeaderFile/mysql_class.h SourceFile/controller_class.cpp SourceFile/dns_class.cpp SourceFile/mysql_class.cpp main.cpp -O3 -lldns -lmysqlclient -o scanDnsToDB
